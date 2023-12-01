a5689de05d09aea1c3a0217bdabc9f6086a9d77c
Kai Alcantara <kai.alcantara95@gmail.com>
2023-11-30 22:00:18 -0800

    clients routed

---
+++ controllers/ProfileController.js
@@ -0,0 +1,106 @@
+const Profile = require("../models/Profile.js");
+
+const ProfileOps = require("../data/ProfileOps");
+// instantiate the class so we can use its methods
+const _profileOps = new ProfileOps();
+
+exports.Index = async function (request, response) {
+  console.log("loading profiles from controller");
+  let profiles = await _profileOps.getAllProfiles();
+  if (profiles) {
+    response.render("profiles", {
+      title: "Express Yourself - Profiles",
+      profiles: profiles,
+      errorMessage: "",
+    });
+  } else {
+    response.render("profiles", {
+      title: "Express Yourself - Profiles",
+      profiles: [],
+      errorMessage: "",
+    });
+  }
+};
+
+exports.Detail = async function (request, response) {
+  const profileId = request.params.id;
+  console.log(`loading single profile by id ${profileId}`);
+  let profile = await _profileOps.getProfileById(profileId);
+  let profiles = await _profileOps.getAllProfiles();
+  if (profile) {
+    response.render("profile", {
+      title: "Express Yourself - " + profile.name,
+      profiles: profiles,
+      profileId: request.params.id,
+      layout: "./layouts/sidebar",
+    });
+  } else {
+    response.render("profiles", {
+      title: "Express Yourself - Profiles",
+      profiles: [],
+    });
+  }
+};
+
+// Handle profile form GET request
+exports.Create = async function (request, response) {
+  response.render("profile-create", {
+    title: "Create Profile",
+    errorMessage: "",
+    profile: {},
+  });
+};
+
+// Handle profile form GET request
+exports.CreateProfile = async function (request, response) {
+  // instantiate a new Profile Object populated with form data
+  let tempProfileObj = new Profile({
+    name: request.body.name,
+  });
+
+  //
+  let responseObj = await _profileOps.createProfile(tempProfileObj);
+
+  // if no errors, save was successful
+  if (responseObj.errorMsg == "") {
+    let profiles = await _profileOps.getAllProfiles();
+    console.log(responseObj.obj);
+    response.render("profile", {
+      title: "Express Yourself - " + responseObj.obj.name,
+      profiles: profiles,
+      profileId: responseObj.obj._id.valueOf(),
+      layout: "./layouts/sidebar",
+    });
+  }
+  // There are errors. Show form the again with an error message.
+  else {
+    console.log("An error occured. Item not created.");
+    response.render("profile-create", {
+      title: "Create Profile",
+      profile: responseObj.obj,
+      errorMessage: responseObj.errorMsg,
+    });
+  }
+};
+
+// Handle profile form GET request
+exports.DeleteProfileById = async function (request, response) {
+  const profileId = request.params.id;
+  console.log(`deleting single profile by id ${profileId}`);
+  let deletedProfile = await _profileOps.deleteProfileById(profileId);
+  let profiles = await _profileOps.getAllProfiles();
+
+  if (deletedProfile) {
+    response.render("profiles", {
+      title: "Express Yourself - Profiles",
+      profiles: profiles,
+      errorMessage: "",
+    });
+  } else {
+    response.render("profiles", {
+      title: "Express Yourself - Profiles",
+      profiles: profiles,
+      errorMessage: "Error.  Unable to Delete",
+    });
+  }
+};
---
+++ models/Profile.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const profileSchema = mongoose.Schema(
+  {
+    // MongoDB will automatically create an _id property, so no need to specify it UNLESS we want to create our own
+
+    // We want every profile document to have a name which should be a string and required
+    name: { type: "String", required: true },
+
+    // interests are optional, so lets just specify that they must be of type Array
+    interests: Array,
+  },
+  // as a second argument, let's specify the collection we want to work with
+  { collection: "profiles" }
+);
+
+// Pass the Schema into Mongoose to use as our model
+const Profile = mongoose.model("Profile", profileSchema);
+
+// Export it so that we can use this model in our App
+module.exports = Profile;
